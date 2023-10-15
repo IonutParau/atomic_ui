a27b26009ebaa27d82f6f4a90c797bc2ce0331f0
IonutParau <ionut.alex.parau@gmail.com>
2023-10-15 12:01:20 +0200

    box

--- atomic_ui/widgets/containers.lua
+++ atomic_ui/widgets/containers.lua
@@ -9,6 +9,7 @@ AtomicUI.Padding = AtomicUI.widget {
       width = child.geometry.width + self.padding * 2,
       height = child.geometry.height + self.padding * 2,
     }
+    self.geometry = config.geometry or self.geometry
     self.geometry.x = config.x or self.geometry.x
     self.geometry.y = config.y or self.geometry.y
     self.geometry.width = config.width or self.geometry.width
@@ -24,5 +25,39 @@ AtomicUI.Padding = AtomicUI.widget {
     child.geometry.y = self.geometry.y + self.padding
     child.geometry.width = self.geometry.width - self.padding * 2
     child.geometry.height = self.geometry.height - self.padding * 2
+  end
+}
+
+AtomicUI.Box = AtomicUI.widget {
+  init = function(self, config)
+    self.padding = config.padding
+    self.color = config.color or AtomicUI.CurrentTheme.secondaryColor or AtomicUI.color()
+    self.rx = config.rx
+    self.ry = config.ry
+    self.segments = config.segments
+
+    self.geometry = config.geometry or self.geometry
+    self.geometry.x = config.x or self.geometry.x
+    self.geometry.y = config.y or self.geometry.y
+    self.geometry.width = config.width or self.geometry.width
+    self.geometry.height = config.height or self.geometry.height
+
+    local child = config[1]
+
+    self:Add(AtomicUI.Padding {
+      child,
+      padding = self.padding,
+      geometry = self.geometry,
+    })
+  end,
+  beginDraw = function(self)
+    local oldcolor = AtomicUI.color()
+
+    love.graphics.rectangle("fill", self.geometry.x, self.geometry.y, self.geometry.width, self.geometry.height, self.rx, self.ry, self.segments)
+
+    oldcolor:apply()
+  end,
+  update = function (self, dt)
+    self.geometry:copyInto(self.subwidget[1].geometry)
   end
 }
